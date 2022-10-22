d7f97cf20a96b994678fe6563e494ebea54e505a
Hridaya Bijayananda:= <hrbcrrvp@gmail.com>
2022-10-21 19:46:09 -0600

    Fibrecurse done

--- CSC2025/FibRecursive/main.asm
+++ CSC2025/FibRecursive/main.asm
@@ -1,26 +1,26 @@
 .386P
 .model flat
 
-extern _ExitProcess@4:near
+extern _ExitProcess@4: near
 extern _GetStdHandle@4: near
-extern _WriteConsoleA@20:near
-extern _ReadConsoleA@20:near
+extern _WriteConsoleA@20: near
+extern _ReadConsoleA@20: near
 extern fibrecurse: near
 
 .data
 
-n   dword   5 ; the nth term
+n			dword   5 ; the nth term
 
 .code
 main PROC near
 _main:
 
-	mov ecx, n
-	push ecx
-	call fibrecurse
+	mov		ecx, n
+	push	ecx
+	call	fibrecurse
 	
-	push  0
-	call  _ExitProcess@4
+	push	0
+	call	_ExitProcess@4
 
 main ENDP
 END
